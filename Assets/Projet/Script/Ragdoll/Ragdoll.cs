78b2d2b9f8dce426d582468bdf1a3a7e1de1e878
Louan Ourvouai <megakitraz3000@gmail.com>
2024-09-23 14:25:21 +0200

    Add mutations

--- Assets/Projet/Script/Ragdoll/Ragdoll.cs
+++ Assets/Projet/Script/Ragdoll/Ragdoll.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Ragdoll", menuName = "Ragdoll")]
@@ -79,14 +80,182 @@ public class Ragdoll : ScriptableObject
 
         if(rand < 15)
         {
-
+            DoMutationChangeCycle();
         }
         else if(rand < 25)
         {
-
+            DoMutationAddCycle();
         }
         else
         {
+            DoMutationChangeMovementArticulation();
+        }
+    }
+
+    private void DoMutationChangeMovementArticulation()
+    {
+        switch (UnityEngine.Random.Range(0, 12))
+        {
+            case 0:
+                leftShoulder_MovementCycle = RandomMovementArticulations(UnityEngine.Random.Range(1, 20));
+                break;
+            case 1:
+                leftUpArm_MovementCycle = RandomMovementArticulations(UnityEngine.Random.Range(1, 20));
+                break;
+            case 2:
+                leftDownArm_MovementCycle = RandomMovementArticulations(UnityEngine.Random.Range(1, 20));
+                break;
+            case 3:
+                rightShoulder_MovementCycle = RandomMovementArticulations(UnityEngine.Random.Range(1, 20));
+                break;
+            case 4:
+                rightUpArm_MovementCycle = RandomMovementArticulations(UnityEngine.Random.Range(1, 20));
+                break;
+            case 5:
+                rightDownArm_MovementCycle = RandomMovementArticulations(UnityEngine.Random.Range(1, 20));
+                break;
+            case 6:
+                leftArticulation_MovementCycle = RandomMovementArticulations(UnityEngine.Random.Range(1, 20));
+                break;
+            case 7:
+                leftUpLeg_MovementCycle = RandomMovementArticulations(UnityEngine.Random.Range(1, 20));
+                break;
+            case 8:
+                leftDownLeg_MovementCycle = RandomMovementArticulations(UnityEngine.Random.Range(1, 20));
+                break;
+            case 9:
+                rightArticulation_MovementCycle = RandomMovementArticulations(UnityEngine.Random.Range(1, 20));
+                break;
+            case 10:
+                rightUpLeg_MovementCycle = RandomMovementArticulations(UnityEngine.Random.Range(1, 20));
+                break;
+            case 11:
+                rightDownLeg_MovementCycle = RandomMovementArticulations(UnityEngine.Random.Range(1, 20));
+                break;
+            case 12:
+                head_MovementCycle = RandomMovementArticulations(UnityEngine.Random.Range(1, 20));
+                break;
+
+        }
+    }
+
+    private void DoMutationAddCycle()
+    {
+        switch (UnityEngine.Random.Range(0, 12))
+        {
+            case 0:
+                leftShoulder_MovementCycle.Append<MovementArticulation>(new MovementArticulation(UnityEngine.Random.Range(0.1f, 3f), UnityEngine.Random.insideUnitSphere * 5f));
+                break;
+            case 1:
+                leftUpArm_MovementCycle.Append<MovementArticulation>(new MovementArticulation(UnityEngine.Random.Range(0.1f, 3f), UnityEngine.Random.insideUnitSphere * 5f));
+                break;
+            case 2:
+                leftDownArm_MovementCycle.Append<MovementArticulation>(new MovementArticulation(UnityEngine.Random.Range(0.1f, 3f), UnityEngine.Random.insideUnitSphere * 5f));
+                break;
+            case 3:
+                rightShoulder_MovementCycle.Append<MovementArticulation>(new MovementArticulation(UnityEngine.Random.Range(0.1f, 3f), UnityEngine.Random.insideUnitSphere * 5f));
+                break;
+            case 4:
+                rightUpArm_MovementCycle.Append<MovementArticulation>(new MovementArticulation(UnityEngine.Random.Range(0.1f, 3f), UnityEngine.Random.insideUnitSphere * 5f));
+                break;
+            case 5:
+                rightDownArm_MovementCycle.Append<MovementArticulation>(new MovementArticulation(UnityEngine.Random.Range(0.1f, 3f), UnityEngine.Random.insideUnitSphere * 5f));
+                break;
+            case 6:
+                leftArticulation_MovementCycle.Append<MovementArticulation>(new MovementArticulation(UnityEngine.Random.Range(0.1f, 3f), UnityEngine.Random.insideUnitSphere * 5f));
+                break;
+            case 7:
+                leftUpLeg_MovementCycle.Append<MovementArticulation>(new MovementArticulation(UnityEngine.Random.Range(0.1f, 3f), UnityEngine.Random.insideUnitSphere * 5f));
+                break;
+            case 8:
+                leftDownLeg_MovementCycle.Append<MovementArticulation>(new MovementArticulation(UnityEngine.Random.Range(0.1f, 3f), UnityEngine.Random.insideUnitSphere * 5f));
+                break;
+            case 9:
+                rightArticulation_MovementCycle.Append<MovementArticulation>(new MovementArticulation(UnityEngine.Random.Range(0.1f, 3f), UnityEngine.Random.insideUnitSphere * 5f));
+                break;
+            case 10:
+                rightUpLeg_MovementCycle.Append<MovementArticulation>(new MovementArticulation(UnityEngine.Random.Range(0.1f, 3f), UnityEngine.Random.insideUnitSphere * 5f));
+                break;
+            case 11:
+                rightDownLeg_MovementCycle.Append<MovementArticulation>(new MovementArticulation(UnityEngine.Random.Range(0.1f, 3f), UnityEngine.Random.insideUnitSphere * 5f));
+                break;
+            case 12:
+                head_MovementCycle.Append<MovementArticulation>(new MovementArticulation(UnityEngine.Random.Range(0.1f, 3f), UnityEngine.Random.insideUnitSphere * 5f));
+                break;
+
+        }
+    }
+
+    private void DoMutationChangeCycle()
+    {
+        int index;
+        switch (UnityEngine.Random.Range(0, 12))
+        {
+            case 0:
+                index = UnityEngine.Random.Range(0, leftShoulder_MovementCycle.Length);
+                leftShoulder_MovementCycle[index].time = UnityEngine.Random.Range(0.1f, 3f);
+                leftShoulder_MovementCycle[index].strength = UnityEngine.Random.insideUnitSphere * 5f;
+                break;
+            case 1:
+                index = UnityEngine.Random.Range(0, leftUpArm_MovementCycle.Length);
+                leftUpArm_MovementCycle[index].time = UnityEngine.Random.Range(0.1f, 3f);
+                leftUpArm_MovementCycle[index].strength = UnityEngine.Random.insideUnitSphere * 5f;
+                break;
+            case 2:
+                index = UnityEngine.Random.Range(0, leftDownArm_MovementCycle.Length);
+                leftDownArm_MovementCycle[index].time = UnityEngine.Random.Range(0.1f, 3f);
+                leftDownArm_MovementCycle[index].strength = UnityEngine.Random.insideUnitSphere * 5f;
+                break;
+            case 3:
+                index = UnityEngine.Random.Range(0, rightShoulder_MovementCycle.Length);
+                rightShoulder_MovementCycle[index].time = UnityEngine.Random.Range(0.1f, 3f);
+                rightShoulder_MovementCycle[index].strength = UnityEngine.Random.insideUnitSphere * 5f;
+                break;
+            case 4:
+                index = UnityEngine.Random.Range(0, rightUpArm_MovementCycle.Length);
+                rightUpArm_MovementCycle[index].time = UnityEngine.Random.Range(0.1f, 3f);
+                rightUpArm_MovementCycle[index].strength = UnityEngine.Random.insideUnitSphere * 5f;
+                break;
+            case 5:
+                index = UnityEngine.Random.Range(0, rightDownArm_MovementCycle.Length);
+                rightDownArm_MovementCycle[index].time = UnityEngine.Random.Range(0.1f, 3f);
+                rightDownArm_MovementCycle[index].strength = UnityEngine.Random.insideUnitSphere * 5f;
+                break;
+            case 6:
+                index = UnityEngine.Random.Range(0, leftArticulation_MovementCycle.Length);
+                leftArticulation_MovementCycle[index].time = UnityEngine.Random.Range(0.1f, 3f);
+                leftArticulation_MovementCycle[index].strength = UnityEngine.Random.insideUnitSphere * 5f;
+                break;
+            case 7:
+                index = UnityEngine.Random.Range(0, leftUpLeg_MovementCycle.Length);
+                leftUpLeg_MovementCycle[index].time = UnityEngine.Random.Range(0.1f, 3f);
+                leftUpLeg_MovementCycle[index].strength = UnityEngine.Random.insideUnitSphere * 5f;
+                break;
+            case 8:
+                index = UnityEngine.Random.Range(0, leftDownLeg_MovementCycle.Length);
+                leftDownLeg_MovementCycle[index].time = UnityEngine.Random.Range(0.1f, 3f);
+                leftDownLeg_MovementCycle[index].strength = UnityEngine.Random.insideUnitSphere * 5f;
+                break;
+            case 9:
+                index = UnityEngine.Random.Range(0, rightArticulation_MovementCycle.Length);
+                rightArticulation_MovementCycle[index].time = UnityEngine.Random.Range(0.1f, 3f);
+                rightArticulation_MovementCycle[index].strength = UnityEngine.Random.insideUnitSphere * 5f;
+                break;
+            case 10:
+                index = UnityEngine.Random.Range(0, rightUpLeg_MovementCycle.Length);
+                rightUpLeg_MovementCycle[index].time = UnityEngine.Random.Range(0.1f, 3f);
+                rightUpLeg_MovementCycle[index].strength = UnityEngine.Random.insideUnitSphere * 5f;
+                break;
+            case 11:
+                index = UnityEngine.Random.Range(0, rightDownLeg_MovementCycle.Length);
+                rightDownLeg_MovementCycle[index].time = UnityEngine.Random.Range(0.1f, 3f);
+                rightDownLeg_MovementCycle[index].strength = UnityEngine.Random.insideUnitSphere * 5f;
+                break;
+            case 12:
+                index = UnityEngine.Random.Range(0, head_MovementCycle.Length);
+                head_MovementCycle[index].time = UnityEngine.Random.Range(0.1f, 3f);
+                head_MovementCycle[index].strength = UnityEngine.Random.insideUnitSphere * 5f;
+                break;
 
         }
     }
